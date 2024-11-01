b41e100a710dc5bfb8db23872089be8f2116d05f
GK-Sabermetrics <kempgarrettm@gmail.com>
2024-11-01 08:25:10 -0400

    added labeles to app sections and reformatted code

--- MUBSBL/ui.R
+++ MUBSBL/ui.R
@@ -16,32 +16,37 @@ library(shinythemes)
 
 
 page_navbar(
-  nav_panel("Home",
-            accordion(open = FALSE,
-              accordion_panel(title = 'Updates',
-                              icon = bs_icon('bell')
-                              ),
-              accordion_panel(title = 'Info',
-                              icon = bs_icon('info-square')
-              )
-            )
-            ),
+  # Home Page ----
+  nav_panel(
+    "Home",
+    accordion(
+      open = FALSE,
+      accordion_panel(title = 'Updates', icon = bs_icon('bell')),
+      accordion_panel(title = 'Info', icon = bs_icon('info-square'))
+    )
+  ),
+  # Team Pitching Page ----
   nav_panel("Team Pitching", "Team Pitching Content"),
+  # Team Hitting Page ----
   nav_panel("Team Hitting", "Team Hitting Content"),
-  nav_panel("Pitching", 
-            layout_sidebar(sidebar = sidebar(width = 250, open = 'open', title = 'Filters',
-                                             div(fluidRow(
-                                                          column(6, checkboxGroupInput("ball", "Balls", c('a'))),
-                                                          column(6, checkboxGroupInput("strike", "Balls", c('b')))
-                                                        )
-                                                ),
-                                            ),
-            
-                          ),
-            ), 
+  # Pitching Page ----
+  nav_panel("Pitching", layout_sidebar(
+    sidebar = sidebar(
+      width = 250,
+      open = 'open',
+      title = 'Filters',
+      div(fluidRow(
+        column(6, checkboxGroupInput("ball", "Balls", c('a'))),
+        column(6, checkboxGroupInput("strike", "Balls", c('b')))
+      )),
+    ),
+  ), ),
+  # Hitting Page ----
   nav_panel("Hitting", "Page C content"),
+  # Game Reports Page ----
   nav_panel("Game Reports", "Game reports content"),
+  # Umpire Reports Page ----
   nav_panel("Umpire Reports", "Umpire reports  content"),
-  title = "MU Baseball", 
-  id = "page", 
+  title = "MU Baseball",
+  id = "page",
 )
